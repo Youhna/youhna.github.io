da4dc4b81f323e6c2262b95f70bb9a5d162111ac
Youhan <s3987940@student.rmit.edu.au>
2025-04-03 15:40:50 +1100

    ok

---
+++ week5/script.js
@@ -0,0 +1,52 @@
+// console.log("Hello");
+// console.log("How are you.buddy");
+// let myName ="Youhan",
+// console.log("Hello",myNAme);
+// let id="s3987940";
+// console.log("my id is",id)
+// let myCity=prompt("where do you live?");
+// console.log("I live in",myCity);
+//cmd/
+//this is a comment
+
+let grade = 80
+if (grade> 75){
+    console.log("yep! you got Distinction");
+}else{
+    console.log("You just passed the course");
+}
+{
+    let a =10;
+    console.log("value of a is", a)
+}
+console.log("value of a is", a)
+//numberical variables 0 -9
+let a =10;
+let b=parseInt("20");
+let c=a+b;
+console.log(a,b,c);
+// +add - subtract *multiply / division
+//string or text variables``""``back ticks
+let myName ="Youhan";
+console.log("Hello",myName);
+
+let myCity="Wuhan";
+let msg=<h1> I live in ${myCity}</h1>;
+console.log(msg);
+
+let isItSunday=false;
+const isItOART1013= true;
+//object variable
+const myStudentRecord= {
+    name:"Youhan",
+    id:1234,
+    homeTown:"Wuhan",
+    isLocal:true,
+};
+console.log(myStudentRecord);
+console.log(myStudentRecord,homeTown);
+let student1= "hi";
+let student2= "yi";
+let student3= "wi";
+let student4= "qi";
+let student5= "ti";
